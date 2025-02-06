cb32ff9c16102fa60865878ef22b2808b4c72fea
xsanfz <sergey.myamin@gmail.com>
2025-02-06 17:37:35 +0400

    Add files via upload

--- mail.py
+++ mail.py
@@ -1,12 +1,17 @@
 from idlelib.replace import replace
-
+from email.message import EmailMessage
 my_name = "Сергей"
-
 friend_name = "Дмитрий"
-
 website = "https://dvmn.org/profession-ref-program/sergey.myamin/TK540/"
+letter = EmailMessage()
+letter['From'] = 'rewaqz1@yandex.ru'
+letter['To'] = 'Am1dok@yandex.ru'
+letter['Subject'] = 'Приглашение!'
 
-my_str = """Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
+
+
+
+my_str = ("""Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
 
 %website% — это новая версия онлайн-курса по программированию. 
 Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
@@ -21,7 +26,20 @@ my_str = """Привет, %friend_name%! %my_name% приглашает тебя
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → %website%  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""")
+my_str = my_str.replace('%friend_name%', friend_name)
+
+my_str = my_str.replace('%my_name%',my_name)
+
+my_str = my_str.replace('%website%', website)
+
+my_str = my_str.replace('%friend_name%', friend_name)
+
+letter.set_content(my_str, subtype='plain', charset='utf-8')
+
+
+
+
 
 my_str = my_str.replace('%website%', website)
 
@@ -29,4 +47,4 @@ my_str = my_str.replace('%friend_name%', friend_name)
 
 my_str = my_str.replace('%my_name%',my_name)
 
-print(my_str)
+print(letter.set_content)
